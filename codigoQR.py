ccb1465c316ed6cb6eec0883db810630951062bf
ybello <yoinercoding@gmail.com>
2022-09-07 14:17:24 -0300

    Se agrega script para generar QR WI-FI

--- codigoQR.py
+++ codigoQR.py
@@ -1,13 +1,43 @@
+##########################GENERATOR QR###################################
+
 import qrcode
 from PIL import Image
 
 cadena = input("Introduzca lo que desea Generar a QR: ")
 imagen = qrcode.make(cadena)
 
-nombre_imagen = input("Introduzca el nombre de la imagen: ") + '.pdf'
+nombre_imagen = input("Introduzca el nombre del archivo: ") + '.pdf'
 archivo_imagen = open(nombre_imagen,'wb')
 imagen.save(archivo_imagen)
 archivo_imagen.close()
 
 ruta_imagen = './'+nombre_imagen
 Image.open(ruta_imagen).show()
+
+########################GENERATOR QR WIFI (PROBAR)#####################
+
+"""import subprocess 
+import wifi_qrcode_generator 
+  
+try: 
+    
+    Id = subprocess.check_output( 
+        ['netsh', 'wlan', 'show', 'interfaces']).decode('utf-8').split('\n') 
+      
+    id_results = str([b.split(":")[1][1:-1] 
+                      for b in Id if "Profile" in b])[2:-3] 
+  
+    
+    password = subprocess.check_output( 
+        ['netsh', 'wlan', 'show', 'profiles',  
+         id_results, 'key=clear']).decode('utf-8').split('\n') 
+      
+    pass_results = str([b.split(":")[1][1:-1] 
+                        for b in password if "Key Content" in b])[2:-2] 
+    print("User name :", id_results) 
+    print("Password :", pass_results) 
+      
+except: 
+    print("something wrong") 
+      
+wifi_qrcode_generator.wifi_qrcode(id_results, False, 'WPA', pass_results)"""
